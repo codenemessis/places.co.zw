024c80efe72cc58c3043d5f897f85914f574ee7b
munyahcee <munyaradzichiyanike@gmail.com>
2015-12-17 15:01:46 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+		<html>
+			<head>
+			<title>Places - Celebrate Life</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
+			<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+			<script type="text/javascript" src="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
+		</head>
+		<body>
 
+		<div data-role="page">
+
+			<div data-role="header"data-theme="b">
+				<h1>Settings</h1>
+				<a href="home.php" data-icon="home" data-theme="a">Home</a>
+			</div><!-- /header -->
+
+			<div data-role="content">
+				<ul data-role="listview" data-theme="c" data-filter="true" data-filter-placeholder="Search for Places" data-divider-theme="e">
+					<li><a href="#"><img  class="img" src="images/logo.jpg"/><h3>Select town or city</h3></a></li>
+					<li><a href="#"><img class="img" src="images/logo.jpg"/><h3>About Places</h3></a></li>
+					<li><a href="#"><img class="img" src="images/logo.jpg"/><h3>Help</h3></a></li>
+				</ul>
+			</div><!-- /content -->
+
+			<div data-role="footer"data-theme="b">
+				<h1>&copy Places Zimbabwe</h1>
+			</div><!-- /footer -->
+
+		</div><!-- /page -->
+		</body>
+		</html>

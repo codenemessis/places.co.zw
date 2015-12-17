c81440e97227df617f837657c00fb4a59f6ad0f7
munyahcee <munyaradzichiyanike@gmail.com>
2015-12-17 15:03:48 +0200

    Update buzz.php

--- buzz.php
+++ buzz.php
@@ -1,1 +1,35 @@
+<!DOCTYPE html>
+		<html>
+			<head>
+			<title>Places - Celebrate Life</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
+			<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+			<script type="text/javascript" src="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
+		</head>
+		<body>
 
+		<div data-role="page">
+
+			<div data-role="header"data-theme="b">
+				<h1>Buzz</h1>
+				<a href="home.php" data-icon="home" data-theme="a">Home</a>
+			</div><!-- /header -->
+
+			<div data-role="content">
+				<ul data-role="listview" data-theme="c" data-filter="true" data-filter-placeholder="Search for Places" data-divider-theme="e">
+					<li><a href="#"><img  class="img" src="images/logo.jpg"/><h3>Taxi Check</h3><p>City Cab Verification</p></a></li>
+					<li><a href="#"><img class="img" src="images/logo.jpg"/><h3>Castle Premier Soccer League</h3><p>Results, Fixtures, Log</p></a></li>
+					<li><a href="#"><img class="img" src="images/logo.jpg"/><h3>Barclays Premier League</h3><p>Results, Fixtures, Log</p></a></li>
+					<li><a href="#"><img class="img" src="images/logo.jpg"/><h3>My Places</h3><p>Personal planner</p></a></li>
+					<li><a href="#"><img class="img" src="images/logo.jpg"/><h3>My Events</h3><p>Reminders</p></a></li>
+				</ul>
+			</div><!-- /content -->
+
+			<div data-role="footer"data-theme="b">
+				<h1>&copy Places Zimbabwe</h1>
+			</div><!-- /footer -->
+
+		</div><!-- /page -->
+		</body>
+		</html>

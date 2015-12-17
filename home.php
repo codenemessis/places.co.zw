84b84405bd1fb22338ae85922c1a5b429283af48
munyahcee <munyaradzichiyanike@gmail.com>
2015-12-17 14:59:54 +0200

    Update home.php

--- home.php
+++ home.php
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+		<html>
+			<head>
+			<title>Places - Celebrate Life</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
+			<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+			<script type="text/javascript" src="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
+		</head>
+		<body>
 
+		<div data-role="page">
+			<div data-role="header" data-theme="b">
+				<h1>Home</h1>
+			</div><!-- /header -->
+
+			<div data-role="content" id="home">
+				<ul data-role="listview" data-theme="f" data-count-theme="e">
+					<li><a href="places.php"><h2>Places</h2><p>Description</p></a><p class="ui-li-count">24</p></li>
+					<li><a href="artists.php"><h2>Artists</h2><p>Description</p></a><p class="ui-li-count">70</p></li>
+					<li><a href="events.php"><h2>Events</h2><p>Description</p></a><p class="ui-li-count">12</p></li>
+					<li><a href="buzz.php"><h2>Buzz</h2><p>Description</p></a><p class="ui-li-count">7</p></li>
+					<li><a href="settings.php"><h2>Settings</h2><p>Description</p></a></li>
+				</ul>
+			</div><!-- /content -->
+
+			<div data-role="footer"data-theme="b">
+				<h1>&copy Places Zimbabwe</h1>
+			</div><!-- /footer -->
+
+		</div><!-- /page -->
+		</body>
+		</html>

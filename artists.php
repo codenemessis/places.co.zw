bd79c52b127bd43d210e7e1ddfd8b3eda31186d0
munyahcee <munyaradzichiyanike@gmail.com>
2015-12-17 15:02:47 +0200

    Update and rename artists to artists.php

---
+++ artists.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+		<html>
+			<head>
+			<title>Places - Celebrate Life</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
+			<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+			<script type="text/javascript" src="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
+		</head>
+		<body>
+
+		<div data-role="page">
+
+			<div data-role="header"data-theme="b">
+				<h1>Artists</h1>
+				<a href="home.php" data-icon="home" data-theme="a">Home</a>
+			</div><!-- /header -->
+
+			<div data-role="content">
+				<ul data-role="listview" data-theme="c" data-filter="true" data-filter-placeholder="Search for Artists" data-divider-theme="e">
+					<li data-role="list-divider" data-theme="d">A</li>
+					<li><a href="artists/alvink.php"><img  class="img" src="images/logo.jpg"/><h3>Alvin Kay</h3><p>Genre</p></a></li>
+					<li data-role="list-divider" data-theme="d">B</li>
+					<li><a href="artists/benita"><img class="img" src="images/logo.jpg"/><h3>Badman</h3><p>Genre</p></a></li>
+					<li data-role="list-divider" data-theme="d">C</li>
+					<li><a href="artists/benita"><img class="img" src="images/logo.jpg"/><h3>Celsius</h3><p>Genre</p></a></li>
+					<li><a href="artists/benita"><img class="img" src="images/logo.jpg"/><h3>Crystal</h3><p>Genre</p></a></li>
+					<li data-role="list-divider" data-theme="d">D</li>
+					<li><a href="artists/benita"><img class="img" src="images/logo.jpg"/><h3>Dadza D</h3><p>Genre</p></a></li>
+				</ul>
+			</div><!-- /content -->
+
+			<div data-role="footer"data-theme="b">
+				<h1>&copy Places Zimbabwe</h1>
+			</div><!-- /footer -->
+
+		</div><!-- /page -->
+		</body>
+		</html>
